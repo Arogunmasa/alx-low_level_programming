716f69e74346795298964b96c9b4ec2f87c8cb3e
Arogunmasa <moyosore2019.com>
2022-09-08 16:41:05 -0700

    god did

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (success)
  */
 
-int main (void)
+int main(void)
 
 
 
@@ -15,7 +15,7 @@ int main (void)
 
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 
 
 
