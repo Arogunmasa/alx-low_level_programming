08018342729bb515e37db1ccbeb11d9a0d4a8daa
Arogunmasa <moyosore2019.com>
2022-09-08 14:54:12 -0700

    ugh corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #!/bin/bash
-#include<stdio.h>
+#include <stdio.h>
+
+
 
 /**
  *
- *  * main - Entry point
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- *   *
+ *   * Return: 0 if exited properly, non-zero otherwise
  *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *    */
 
 int main(void)
 
