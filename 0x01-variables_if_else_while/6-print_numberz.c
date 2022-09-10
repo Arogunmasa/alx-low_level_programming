b22233d033437ea48cd05f83433fc176b14c7712
Arogunmasa <moyosore2019.com>
2022-09-10 16:47:31 -0700

    something right

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the single digit numbers
+ * followed by a new line, using putchar
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		return (0);
+	}
