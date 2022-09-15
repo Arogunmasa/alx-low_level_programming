5634bab4aeb3550cee0aea803502d5a503f3a9cc
Arogunmasa <moyosore2019.com>
2022-09-15 07:02:46 -0700

    euphoriaaaa

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -19,7 +19,7 @@
  * Return: void.
  */
 
-void print_remaining_days(int month, int day, int year)
+void print_remaining_days(int month, int dsy, int year)
 {
 	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
@@ -27,18 +27,19 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 366 - day);
-}
-else
-{
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-}
-else
-{
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 365 - day);
-}
-}
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31,year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
+	}
 }
