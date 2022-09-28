ff23d1beb27fcc782c61bde131488a21143fa5b4
Arogunmasa <moyosore2019.com>
2022-09-27 21:55:26 -0700

    dhfhe

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,23 +10,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, f, flag;
+	int i, j, flag;
 
 	f = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[1] != '\0'; i++)
 	{
-		if (s[i] == accept[j])
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			f++;
-			flag = 1;
+			if (s[i] == accept[j])
+			{
+				f++;
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+		{
+			return (f);
 		}
 	}
-	if (flag == 0)
-	{
-		return (f);
-	}
-}
 
-return (0);
+	return (0);
 }
